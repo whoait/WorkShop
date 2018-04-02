f1c9fc3cac20dfce854d46d15cacad5ff7cbbd60
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-04-02 21:08:44 +0700

    Set config.generators

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,16 @@ module WorkShop
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.javascripts false
+      g.stylesheets false
+    end
   end
 end
