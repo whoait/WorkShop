7ac787a7f2147b8d7edab8e670431ea5c6ab0f06
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-04-03 21:57:06 +0700

    Edit routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@
 Rails.application.routes.draw do
   resources :developers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :developers, only: [:index, :show]
+    end
+  end
 end
