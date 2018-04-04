3c5bf15b8bee1bc1d93de692e6e218211556de3a
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-04-04 13:36:51 +0700

    Edit api

--- app/controllers/api/v1/developers_controller.rb
+++ app/controllers/api/v1/developers_controller.rb
@@ -1,10 +1,32 @@
 # frozen_string_literal: true
 module Api::V1
   class DevelopersController < Api:: BaseController
-    def index
-      developer = Developer.all
+    before_action :set_developer, only: %i[show]
 
-      render json: developer, include: [:languages, :programming_languages]
+    def index
+      developers = Developer.includes(
+        :developer_programming_languages,
+        :programming_languages,
+        :developer_languages,
+        :languages
+      )
+
+      render json: developers, include: []
+    end
+
+    def show
+        # rubocop:disable Style/GuardClause
+        if stale?(last_modified: @developer.updated_at.utc)
+          cache_options = { compress: true, expires_in: 24.hours, race_condition_ttl: 10 }
+          Rails.cache.fetch("#{@developer.cache_key}/#{__method__}", cache_options) do
+            render json: @developer, include: []
+          end
+        end
+        # rubocop:enable Style/GuardClause
+    end
+
+    def set_developer
+      @developer = Developer.find(params[:id])
     end
   end
 end
