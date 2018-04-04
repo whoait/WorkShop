1cfec7fe8cac1867b644991f22cf9a3ea09c844b
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-04-04 13:53:07 +0700

    Remove unnecessary test case and fix comment

--- spec/system/form/developer_search_spec.rb
+++ spec/system/form/developer_search_spec.rb
@@ -13,51 +13,26 @@ RSpec.describe 'Developer search form', type: :system do
            languages: [@language], programming_languages: [@programming_language]
     visit root_path
   end
-
-  it 'first load page and search with no param' do
-    expect(page).to have_content 'Developers'
-    expect(page).to have_button 'Search'
-    expect(page).to have_content 'hoa@gmail.com'
-    expect(page).to have_content 'hoa1@gmail.com'
-  end
-
-  it 'search with no param' do
-    click_on 'Search'
-    expect(page).to have_content 'hoa@gmail.com'
-    expect(page).to have_content 'hoa1@gmail.com'
-  end
-
-  it 'search with correct param language' do
-    select 'En', from: 'developer_search_form_language_id'
+  
+  it 'search with param language' do
+    select @language.code, from: 'developer_search_form_language_id'
     click_on 'Search'
     expect(page).to have_content 'hoa@gmail.com'
     expect(page).to have_content @programming_language.name
     expect(page).to have_content @language.code
   end
 
-  it 'search with incorrect param language' do
-    select 'Vi', from: 'developer_search_form_language_id'
-    click_on 'Search'
-    expect(page).to have_no_content 'hoa@gmail.com'
-  end
-
-  it 'search with correct param programming language' do
-    select 'Ruby', from: 'developer_search_form_programming_language_id'
+  it 'search with param programming language' do
+    select @programming_language.name, from: 'developer_search_form_programming_language_id'
     click_on 'Search'
     expect(page).to have_content 'hoa@gmail.com'
     expect(page).to have_content @programming_language.name
     expect(page).to have_content @language.code
   end
 
-  it 'search with incorrect param programming language' do
-    select 'Java', from: 'developer_search_form_programming_language_id'
-    click_on 'Search'
-    expect(page).to have_no_content 'hoa@gmail.com'
-  end
-
-  it 'search with correct param language and programming language' do
-    select 'Ruby', from: 'developer_search_form_programming_language_id'
-    select 'En', from: 'developer_search_form_language_id'
+  it 'search with param language and programming language' do
+    select @programming_language.name, from: 'developer_search_form_programming_language_id'
+    select @language.code, from: 'developer_search_form_language_id'
     click_on 'Search'
     expect(page).to have_content 'hoa@gmail.com'
     expect(page).to have_content @programming_language.name
