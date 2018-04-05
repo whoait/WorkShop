2dfe95c3e11cf25e623ec1b0e75d2e7efbb6c76e
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-04-05 09:24:32 +0700

    Make changes according to review

--- app/serializers/language_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class LanguageSerializer < ActiveModel::Serializer
-  attributes :code
-end
--- app/serializers/programming_language_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class ProgrammingLanguageSerializer < ActiveModel::Serializer
-  attributes :name
-end
