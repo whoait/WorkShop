aeb444b3835473ab0d778fdca0970e184d0c5dac
Hoa Vo <hoa.vo@tech.reapra.sg>
2018-04-06 15:04:26 +0700

    Fix comment: N+1 query

--- app/form/developer_search_form.rb
+++ app/form/developer_search_form.rb
@@ -8,9 +8,9 @@ class DeveloperSearchForm
   attribute :programming_language_id, Integer
 
   def search
-    query = Developer.all
-    query = query.includes(:developer_languages).joins(:developer_languages).where(developer_languages: { language_id: language_id }) if language_id.present?
-    query = query.includes(:developer_programming_languages).joins(:developer_programming_languages).where(developer_programming_languages: { programming_language_id: programming_language_id }) if programming_language_id.present?
+    query = Developer.includes(:languages, :programming_languages)
+    query = query.joins(:developer_languages).where(developer_languages: { language_id: language_id }) if language_id.present?
+    query = query.joins(:developer_programming_languages).where(developer_programming_languages: { programming_language_id: programming_language_id }) if programming_language_id.present?
     query.order(created_at: :desc)
   end
 end
