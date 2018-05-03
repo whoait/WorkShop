6d889a0d1e22da861c8ab514e47bd634e3fc4a71
Hoa Vo <whoait@gmail.com>
2018-05-03 21:30:26 +0700

    add config devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   resources :developers
-  root to: 'developers#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
